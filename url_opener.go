12e1fefc7698a3acd0bd302126e17d3f06d5c0d8
Alex Logsdon <logsdonalex11@gmail.com>
2025-01-19 01:39:08 -0500

    consistency

--- url_opener.go
+++ url_opener.go
@@ -6,34 +6,30 @@ import (
 	"net/url"
 )
 
-type Opener interface {
+type URLOpener interface {
 	Open(context.Context, *url.URL) (Store, error)
 }
 
-type URLMux map[string]Opener
+var (
+	urlMux = map[string]URLOpener{}
+)
 
-func (m URLMux) Open(ctx context.Context, s string) (Store, error) {
+func Register(opener URLOpener, scheme string, schemes ...string) {
+	for _, s := range append(schemes, scheme) {
+		urlMux[s] = opener
+	}
+}
+
+func Open(ctx context.Context, s string) (Store, error) {
 	u, err := url.Parse(s)
 	if err != nil {
 		return nil, err
 	}
 
-	opener, ok := m[u.Scheme]
+	opener, ok := urlMux[u.Scheme]
 	if !ok {
 		return nil, fmt.Errorf("no opener registered for scheme %s", u.Scheme)
 	}
 
 	return opener.Open(ctx, u)
-}
-
-var defaultURLMux = URLMux{}
-
-func Register(opener Opener, scheme string, schemes ...string) {
-	for _, s := range append(schemes, scheme) {
-		defaultURLMux[s] = opener
-	}
-}
-
-func Open(ctx context.Context, s string) (Store, error) {
-	return defaultURLMux.Open(ctx, s)
 }
